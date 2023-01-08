4d9360b855dff72ba666bd9469913abd3625e4c6
Raunak876 <109013965+Raunak876@users.noreply.github.com>
2023-01-08 22:40:52 +0530

    Add files via upload

---
+++ egg dragon.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace.std;
+int main()
+{
+	int A,G,B,S;
+	cout<<"Gold-\nSlver-\nWeight of golden egg-\nWeight of silver egg-\n";
+	cin>>A;
+	cin>>G;
+	cin>>B;
+	cin>>S;
+	if(G*A>=S*B)
+	cout<<"Gold";
+	else
+	cout<<"Silver";
+}
+	
+
+
---
+++ mex.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int r,s,x=0;
+	cout<<"enter two numbers:\n";
+	cin>>r>>s;
+	if(x !=r && x!=s)
+	x=s+1;
+	cout<<"Mex of 2 \n";
+	cout<<x;
+}
+
